50fa988e6b7d7d656fc7e601b2b296f1e9d1ce5f
scarter31191 <scarter31191@gmaile.com>
2020-11-27 18:14:06 -0500

    built edit_post_path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,9 +23,17 @@ class PostsController < ApplicationController
     end
 
     def edit
+        @post = Post.find(params[:id])
     end
 
     def update
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to posts_path, :notice => "Sealed Updated"
+        else
+            render "edit"
+        end
     end
 
     def destroy
